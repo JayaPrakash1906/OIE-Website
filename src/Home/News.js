0be3e698ccdb4d07c4df5dc7be2ebdcc5f5f4107
Jaya Prakash M <jayaprakashtrk8@gmail.com>
2025-03-17 12:28:55 +0530

    commit

--- src/Home/News.js
+++ src/Home/News.js
@@ -7,10 +7,19 @@ import img4 from '../Assets/news4.jpeg';
 import img5 from '../Assets/news5.png';
 import img6 from '../Assets/news6.png';
 import img7 from '../Assets/news7.png';
+import img8 from '../Assets/news8.png';
+import img9 from '../Assets/news9.png';
 
 const posts = [
 
-  
+  {
+    id: 1,
+    title: "CFI Open House 2025: A Celebration of Innovation and Entrepreneurship",
+    date: "15th Feb 2025",
+    image: img8,
+    content: "CFI Open House 2025 showcased groundbreaking innovations, entrepreneurial ventures, and inspiring student achievements.",
+    link:"https://www.linkedin.com/posts/office-of-innovation-and-entrepreneurship_startup-products-ideas-activity-7307278723890720768-CpJh?utm_source=share&utm_medium=member_desktop&rcm=ACoAADFikn4B6jA0Ct1Y84TowFZRR2JHg75s9fg"
+  },
   {
     id: 1,
     title: "Stellantis, Raftar & CFI Gift EV to IIT Madras for Innovation & Research",
@@ -54,14 +63,7 @@ const posts = [
     content: "Hosted by the Centre For Innovation (CFI), Research Conclave-2024 will unveil groundbreaking ideas crafted by our talented teams and clubs.",
     link: "https://www.linkedin.com/posts/office-of-innovation-and-entrepreneurship_researchconclave2024-cfi-researchconclave2024-activity-7255831808057999361-CZey?utm_source=share&utm_medium=member_desktop"
   },
-  {
-    id: 6,
-    title: "Investor-Startup meet from IITMIC",
-    date: "18th Sep 2024",
-    image: img2,
-    content: " Our incubated startups—Plenome, Matterize and Interacts AI showcased their innovations and had meaningful one-on-one discussions with the investors.",
-    link: "https://www.linkedin.com/posts/iitm-incubation-cell_iitmic-deeptech-iitmstartups-activity-7251916041113518081-T7z-?utm_source=share&utm_medium=member_desktop"
-  },
+  
   {
     id: 7,
     title: " The Energy Consortium - IIT Madras",
@@ -70,7 +72,22 @@ const posts = [
     content: "Discussions with IIT Madras leaders and visits to labs highlighted advancements in carbon capture, flow batteries, and future electric grids",
     link: "https://www.linkedin.com/posts/the-energy-consortium-iit-madras_eonatiitmadras-sustainablefuture-energytransition-ugcPost-7257334664070135809-vdzj?utm_source=share&utm_medium=member_desktop"
   },
-  
+  {
+    id: 6,
+    title: "NIRMAAN IITM at Demo Day 2024: A Celebration of Innovation & Impact",
+    date: "27th Sep 2024",
+    image: img9,
+    content: " NIRMAAN IITM shines at Demo Day 2024, showcasing innovation, leadership, and deeptech excellence!",
+    link: "https://www.linkedin.com/posts/office-of-innovation-and-entrepreneurship_demoday2024-innovationhub-iot-activity-7247916838716596224-RFf_?utm_source=share&utm_medium=member_desktop&rcm=ACoAADFikn4B6jA0Ct1Y84TowFZRR2JHg75s9fg"
+  },
+  {
+    id: 6,
+    title: "Investor-Startup meet from IITMIC",
+    date: "18th Sep 2024",
+    image: img2,
+    content: " Our incubated startups—Plenome, Matterize and Interacts AI showcased their innovations and had meaningful one-on-one discussions with the investors.",
+    link: "https://www.linkedin.com/posts/iitm-incubation-cell_iitmic-deeptech-iitmstartups-activity-7251916041113518081-T7z-?utm_source=share&utm_medium=member_desktop"
+  },
 ];
 
 function App() {
